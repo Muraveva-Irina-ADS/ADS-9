a8ad1c19ddd25e31498fa179855049de4ddaa012
Muraveva-Irina-ADS <124904325+Muraveva-Irina-ADS@users.noreply.github.com>
2023-05-27 22:21:27 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -7,4 +7,23 @@
 
 BST<std::string> makeTree(const char* filename) {
   // поместите сюда свой код
+  std::string word;
+    BST<std::string> peace;
+    std::ifstream file(filename);
+    if (file) {
+        while (!file.eof()) {
+            char symbol = tolower(file.get());
+            if (isalpha(symbol))
+                word += symbol;
+            else {
+                if (!word.empty())
+                    peace.add(word);
+                word.clear();
+            }
+        }
+    }
+    else 
+        std::cout << "File error!" << std::endl;
+    file.close();
+    return peace;
 }
